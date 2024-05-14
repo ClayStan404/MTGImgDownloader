cdea00937318659b8d04ec8b3d579c88f7659a92
Clay Stan <claystan97@gmail.com>
2024-05-14 15:37:03 +0800

    Update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # MTG Image Downloader
-Download specific MTG card image from [Scryfall](https://scryfall.com/) and [MTGPICS](https://www.mtgpics.com/)
+Download specific MTG card image from [Scryfall](https://scryfall.com/)
 
 And convert it to a one-to-one size to meet the main image requirements of the damn Taobao.
 
--- src/getMTGPic.py
+++ src/getMTGPic.py
@@ -17,25 +17,35 @@ def download_image(url, filename):
         print('Download faild:', response.status_code)
 
 
+def get_zhs_cardName(url, cardSet, cardNo):
+    response = requests.get(url)
+    if response.status_code != 200:
+        cardNameZh = ''
+        return cardNameZh
+    soup = BeautifulSoup(response.content, 'html.parser')
+    cardNameZh = soup.find_all('title')[0].text.split(
+        '·')[0].split('//')[0]+cardSet+' '+cardNo
+    return cardNameZh
+
+
 def get_image_url(url, cardSet, cardNo):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    cardName = soup.find_all('title')[0].text.split(
-        '·')[0].split('//')[0]+cardSet+'_'+cardNo
     image_elements = soup.find_all('img')
     image_url = image_elements[0]['src']
     if image_url.startswith('http'):
         fileName = '_'.join(
             image_elements[0]['alt'].split('(')[0].split()).split('_//')[0]+'-'+cardSet+'-'+cardNo+'.jpg'
         download_image(image_url, fileName)
-        print("download from scryfall: ", cardName)
+        cardNameEn = fileName.strip('.jpg').replace('-', ' ').replace('_', ' ')
+        #  print("download: ", cardNameEn)
     else:
-        print('faild parse url from scryfall,try it again from mtgpics')
-        image_url = 'https://www.mtgpics.com/pics/big/'+cardSet+'/'+cardNo+'.jpg'
-        fileName = cardSet+'_'+cardNo+'.jpg'
-        download_image(image_url, fileName)
-        print("download from mtgpics: ", cardName)
-    return cardName
+        cardNameEn = "default "+cardSet+' '+cardNo
+        print('faild parse url from scryfall: ', cardNameEn)
+        #  image_url = 'https://www.mtgpics.com/pics/big/'+cardSet+'/'+cardNo+'.jpg'
+        #  download_image(image_url, fileName)
+        #  print("download from mtgpics: ")
+    return cardNameEn
 
 
 def main():
@@ -43,21 +53,41 @@ def main():
     with open("MTGCardList", "r") as f:
         for line in f:
             cardInfo = line.split()
-            if len(cardInfo) == 2:
+            if len(cardInfo) == 3:
+                cardSet, cardNo, cardLang = cardInfo
+            elif len(cardInfo) == 2:
                 cardSet, cardNo = cardInfo
-                try:
-                    url = baseurl+'/'+cardSet+'/'+cardNo+"/zhs"
-                    cardName = get_image_url(url, cardSet, cardNo)
-                except IndexError:
-                    url = baseurl+'/'+cardSet+'/'+cardNo
-                    cardName = get_image_url(url, cardSet, cardNo)
-                except Exception as e:
-                    raise e
-                shellCMD = 'echo "'+cardName+'" >> cardList'
-                os.system(shellCMD)
+                cardLang = ''
             else:
                 print("illegal line: ", line)
                 continue
+            match cardLang:
+                case 'z':
+                    cardUrlLang = "/zhs"
+                case 'f':
+                    cardUrlLang = "/fr"
+                case 'd':
+                    cardUrlLang = "/de"
+                case 'j':
+                    cardUrlLang = "/ja"
+                case _:
+                    cardUrlLang = ""
+
+            try:
+                url = baseurl+'/'+cardSet+'/'+cardNo+cardUrlLang
+                cardNameEn = get_image_url(url, cardSet, cardNo)
+            except IndexError:
+                url = baseurl+'/'+cardSet+'/'+cardNo
+                cardNameEn = get_image_url(url, cardSet, cardNo)
+            except Exception as e:
+                raise e
+            zhsUrl = baseurl+'/'+cardSet+'/'+cardNo+'/zhs'
+            cardName = get_zhs_cardName(zhsUrl, cardSet, cardNo)
+            if cardName == "":
+                cardName = cardNameEn
+            print("download: ", cardName)
+            shellCMD = 'echo "'+cardName+'" >> cardList'
+            os.system(shellCMD)
 
 
 if __name__ == '__main__':
